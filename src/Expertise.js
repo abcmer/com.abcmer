39cdaa19e4733531ff87d86960fca3f1c4a98d94
Adam Siemer <absiemer22@gmail.com>
2018-07-05 16:41:36 -0500

    update some logos for expertise page.

--- src/Expertise.js
+++ src/Expertise.js
@@ -7,110 +7,107 @@ import FaBarChart from 'react-icons/lib/fa/bar-chart';
 import FaServer from 'react-icons/lib/fa/server';
 import FaDatabase from 'react-icons/lib/fa/database';
 import FaWrench from 'react-icons/lib/fa/wrench';
+import FaStar from 'react-icons/lib/fa/star';
 
 import ExpertiseCard from './ExpertiseCard';
 
-import Python from './static/logos/python.png';
-import NodeJS from './static/logos/nodeJS.png';
-// const NodeJS = );
-
 import 'bootstrap/dist/css/bootstrap.css';
 
-export const getFaMusic = () => <FaMusic/>;
-export const getFaCode = () => <FaCode/>;
-export const getFaBarChart = () => <FaBarChart/>;
-export const getFaServer = () => <FaServer/>;
-export const getFaDatabase = () => <FaDatabase/>;
-export const getFaWrench = () => <FaWrench/>;
+const getFaMusic = () => <FaMusic/>;
+const getFaCode = () => <FaCode/>;
+const getFaServer = () => <FaServer/>;
+const getFaDatabase = () => <FaDatabase/>;
+const getFaWrench = () => <FaWrench/>;
+const getFaStar = () => <FaStar/>;
 
 const skills = [{
   'name': 'Python',
   'imgSrc': require('./static/logos/python.png'),
-  'skillCategory': 'language',
+  'skillCategory': 'languages-frameworks',
 }, {
   'name': 'JavaScript',
   'imgSrc': require('./static/logos/javascript.jpeg'),
-  'skillCategory': 'language',
-}, {
-  'name': 'HTML',
-  'imgSrc': require('./static/logos/html5.png'),
-  'skillCategory': 'language'
-}, {
-  'name': 'CSS',
-  'imgSrc': require('./static/logos/css.jpg'),
-  'skillCategory': 'language'
+  'skillCategory': 'languages-frameworks',
 }, {
   'name': 'ReactJS',
   'imgSrc': require('./static/logos/react.png'),
-  'skillCategory': 'frontend-dataviz'
+  'skillCategory': 'languages-frameworks'
+}, {
+  'name': 'HTML',
+  'imgSrc': require('./static/logos/html5.png'),
+  'skillCategory': 'languages-frameworks'
+}, {
+  'name': 'CSS',
+  'imgSrc': require('./static/logos/css.png'),
+  'skillCategory': 'languages-frameworks'
 }, {
   'name': 'Bootstrap',
   'imgSrc': require('./static/logos/bootstrap.png'),
-  'skillCategory': 'frontend-dataviz'
+  'skillCategory': 'languages-frameworks'
 }, {
   'name': 'Flask',
   'imgSrc': require('./static/logos/flask.jpg'),
-  'skillCategory': 'frontend-dataviz'
-}, {
-  'name': 'NodeJS',
-  'imgSrc': require('./static/logos/nodeJS.png'),
-  'skillCategory': 'web-development'
-}, {
-  'name': 'Docker',
-  'imgSrc': require('./static/logos/docker.png'),
-  'skillCategory': 'web-development'
-}, {
-  'name': 'Gunicorn',
-  'imgSrc': require('./static/logos/gunicorn.png'),
-  'skillCategory': 'web-development'
-}, {
-  'name': 'Nginx',
-  'imgSrc': require('./static/logos/nginx.png'),
-  'skillCategory': 'web-development'
-}, {
-  'name': 'uWSGI',
-  'imgSrc': require('./static/logos/uwsgi.png'),
-  'skillCategory': 'web-development'
+  'skillCategory': 'languages-frameworks'
 }, {
   'name': 'MySQL',
   'imgSrc': require('./static/logos/mysql.png'),
-  'skillCategory': 'data'
+  'skillCategory': 'data-storage-viz'
 }, {
   'name': 'MongoDB',
   'imgSrc': require('./static/logos/mongo.png'),
-  'skillCategory': 'data'
+  'skillCategory': 'data-storage-viz'
 }, {
   'name': 'MS SQL Server',
   'imgSrc': require('./static/logos/sqlServer.png'),
-  'skillCategory': 'data'
+  'skillCategory': 'data-storage-viz'
 }, {
   'name': 'Pandas',
   'imgSrc': require('./static/logos/pandas.png'),
-  'skillCategory': 'data'
+  'skillCategory': 'data-storage-viz'
 }, {
   'name': 'Postgres',
   'imgSrc': require('./static/logos/postgres.png'),
-  'skillCategory': 'data'
+  'skillCategory': 'data-storage-viz'
 }, {
   'name': 'Sql Alchemy',
   'imgSrc': require('./static/logos/sqlAlchemy.png'),
-  'skillCategory': 'utility'
+  'skillCategory': 'data-storage-viz'
 }, {
   'name': 'Tableau',
   'imgSrc': require('./static/logos/tableau.gif'),
-  'skillCategory': 'frontend-dataviz'
+  'skillCategory': 'data-storage-viz'
 }, {
   'name': 'Qlik',
   'imgSrc': require('./static/logos/qlik.png'),
-  'skillCategory': 'frontend-dataviz'
+  'skillCategory': 'data-storage-viz'
 }, {
   'name': 'D3',
   'imgSrc': require('./static/logos/d3Js.jpeg'),
-  'skillCategory': 'frontend-dataviz'
+  'skillCategory': 'data-storage-viz'
 }, {
   'name': 'Zoomdata',
   'imgSrc': require('./static/logos/zoomdata.jpg'),
-  'skillCategory': 'data'
+  'skillCategory': 'data-storage-viz'
+}, {
+  'name': 'NodeJS',
+  'imgSrc': require('./static/logos/nodeJS.png'),
+  'skillCategory': 'backend-devops'
+}, {
+  'name': 'Docker',
+  'imgSrc': require('./static/logos/docker.png'),
+  'skillCategory': 'backend-devops'
+}, {
+  'name': 'Gunicorn',
+  'imgSrc': require('./static/logos/gunicorn.png'),
+  'skillCategory': 'backend-devops'
+}, {
+  'name': 'Nginx',
+  'imgSrc': require('./static/logos/nginx.png'),
+  'skillCategory': 'backend-devops'
+}, {
+  'name': 'uWSGI',
+  'imgSrc': require('./static/logos/uwsgi.png'),
+  'skillCategory': 'backend-devops'
 }, {
   'name': 'Git',
   'imgSrc': require('./static/logos/git.png'),
@@ -118,6 +115,10 @@ const skills = [{
 }, {
   'name': 'Trello',
   'imgSrc': require('./static/logos/trello.png'),
+  'skillCategory': 'utility'
+}, {
+  'name': 'Jira',
+  'imgSrc': require('./static/logos/jira.png'),
   'skillCategory': 'utility'
 }, {
   'name': 'Sublime',
@@ -136,51 +137,60 @@ const skills = [{
   'imgSrc': require('./static/logos/postman.png'),
   'skillCategory': 'utility'
 }, {
-  'name': 'Adobe Photoshop',
-  'imgSrc': require('./static/logos/photoshop.jpg'),
-  'skillCategory': 'utility'
-}, {
   'name': 'Saxophone',
   'imgSrc': require('./static/logos/saxophone.png'),
-  'skillCategory': 'creative'
+  'skillCategory': 'creativity'
 }, {
   'name': 'Piano',
   'imgSrc': require('./static/logos/piano.png'),
-  'skillCategory': 'creative'
+  'skillCategory': 'creativity'
 }, {
   'name': 'Film',
   'imgSrc': require('./static/logos/film.png'),
-  'skillCategory': 'creative'
-}];
+  'skillCategory': 'creativity'
+}, {
+  'name': 'Graphic Design',
+  'imgSrc': require('./static/logos/graphic-design.png'),
+  'skillCategory': 'creativity'
+}, {
+  'name': 'Graphic Design',
+  'imgSrc': require('./static/logos/graphic-design.png'),
+  'skillCategory': 'soft-skills'
+}, {
+  'name': 'Graphic Design',
+  'imgSrc': require('./static/logos/graphic-design.png'),
+  'skillCategory': 'soft-skills'
+}, {
+  'name': 'Graphic Design',
+  'imgSrc': require('./static/logos/graphic-design.png'),
+  'skillCategory': 'soft-skills'
+}, {
+  'name': 'Graphic Design',
+  'imgSrc': require('./static/logos/graphic-design.png'),
+  'skillCategory': 'soft-skills'
+}, ];
 
 const skillCategories = [{
-  name: 'Languages',
+  name: 'Languages & Frameworks',
   background: 'white',
   getIcon: getFaCode,
   imgBorderColor: '#015389',
   yearsExperience: '3',
-  skills: skills.filter(skill => skill.skillCategory === 'language'),
+  skills: skills.filter(skill => skill.skillCategory === 'languages-frameworks'),
 }, {
-  name: 'Frontend / Data Viz',
+  name: 'Data Storage & Visualization',
   background: 'white',
-  getIcon: getFaBarChart,
+  getIcon: getFaDatabase,
   imgBorderColor: '#9D4F09',
   yearsExperience: '1',
-  skills: skills.filter(skill => skill.skillCategory === 'frontend-dataviz')
+  skills: skills.filter(skill => skill.skillCategory === 'data-storage-viz')
 }, {
   name: 'Backend / DevOps',
   background: 'white',
   getIcon: getFaServer,
   imgBorderColor: '#086C32',
   yearsExperience: '5',
-  skills: skills.filter(skill => skill.skillCategory === 'web-development')
-}, {
-  name: 'Database',
-  background: 'white',
-  getIcon: getFaDatabase,
-  imgBorderColor: '#9D4F09',
-  yearsExperience: '1',
-  skills: skills.filter(skill => skill.skillCategory === 'data')
+  skills: skills.filter(skill => skill.skillCategory === 'backend-devops')
 }, {
   name: 'Utility',
   background: 'white',
@@ -194,7 +204,7 @@ const skillCategories = [{
   getIcon: getFaMusic,
   imgBorderColor: '#9D4F09',
   yearsExperience: '1',
-  skills: skills.filter(skill => skill.skillCategory === 'creative')
+  skills: skills.filter(skill => skill.skillCategory === 'creativity')
 }
 ];
 
