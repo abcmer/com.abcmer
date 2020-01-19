7f85f25ede0b8dd1395b190aa7b462362452cd91
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2020-01-19 14:31:53 -0600

    Redesign (#4)
    * update photography resizing css
    
    * add projects

---
+++ src/Projects.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "./Projects.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default class Projects extends React.Component {
+  render() {
+    const projects = [
+      {
+        name: "Snake",
+        description: "Simple snake clone",
+        github: "https://github.com/abcmer/snake",
+        link: "https://snake.abcmer.com/",
+        imgSrc: require("./static/project-thumbnails/snake.png"),
+        imgAlt: "snake game"
+      },
+      {
+        name: "Fortunes",
+        description: "All the fortune cookies you desire, without getting full",
+        github: "https://github.com/abcmer/fortunes",
+        link: "https://www.fortunes.abcmer.com/",
+        imgSrc: require("./static/project-thumbnails/fortunes.png"),
+        imgAlt: "fortunes app"
+      },
+    ];
+    return (
+      <Container id="projects" className="projects">
+        <Row>
+          {projects.map(project => {
+            return (
+              <Col sm={{ size: 3, offset: 1 }}>
+                <div>
+                  <h3>{project.name}</h3>
+                  <img src={project.imgSrc} alt={project.imgAlt} />
+                </div>
+                <Row>
+                  <Col />
+                  <Col>
+                    <a href={project.github}>Github</a>
+                  </Col>
+                  <Col>
+                    <a href={project.link}>Link</a>
+                  </Col>
+                  <Col />
+                </Row>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    );
+  }
+}
