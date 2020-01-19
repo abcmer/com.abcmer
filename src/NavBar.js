f5c83e73e3b16c697a92fb740bb3c344add0ed7b
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2020-01-19 17:42:37 -0600

    fix css on NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,45 @@
-import React from "react";
-import { Collapse, Navbar, NavbarToggler, Nav, NavItem } from "reactstrap";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, Nav, NavItem } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
 
 import "./NavBar.css";
 
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  // const navItemStyle = {
+  //   color: "white"
+  // };
+
+  return (
+    <div className="nav-container sticky">
+      <Navbar color="black" light expand="md">
+        <NavbarBrand>
+          <Link to="/">
+            Adam Siemer
+          </Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-sm-0 " navbar>
+            <NavItem>
+              <NavLink href="/projects">
+                Projects
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://drive.google.com/file/d/0B-R24Rv9imxkd3dnc3BYV1ZjeW8/view?usp=sharing">
+                Resume
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div >
+  );
+}
 export default class NavBar extends React.Component {
   constructor(props) {
     super(props);
@@ -26,14 +62,15 @@ export default class NavBar extends React.Component {
     return (
       <div className="nav-container sticky">
         <Navbar color="black" light expand="md">
+          <NavItem>
+            <Link to="/">
+              Adam Siemer
+                </Link>
+          </NavItem>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-sm-0 " navbar>
-              <NavItem>
-                <Link to="/">
-                  Adam Siemer
-                </Link>
-              </NavItem>
+
               <NavItem style={navItemStyle}>
                 <Link to="/projects">
                   Projects
