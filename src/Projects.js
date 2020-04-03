c3ff4e69c5e2b910084358b4d3b8704f1ae23884
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2020-04-03 16:19:02 -0500

    add allwork-noplay project (#10)

--- src/Projects.js
+++ src/Projects.js
@@ -22,6 +22,14 @@ export default class Projects extends React.Component {
         imgSrc: require("./static/project-thumbnails/fortunes.png"),
         imgAlt: "fortunes app"
       },
+      {
+        name: "All Work, No Play",
+        description: "All work and no play makes Adam a dull boy",
+        github: "https://github.com/abcmer/allwork-noplay",
+        link: "https://www.allwork-noplay.abcmer.com/?name=Adam&gender=boy",
+        imgSrc: require("./static/project-thumbnails/allwork-noplay.png"),
+        imgAlt: "all work, no play app"
+      },
     ];
     return (
       <Container id="projects" className="projects fade-in">
